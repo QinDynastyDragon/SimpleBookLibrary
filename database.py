39d5bf014cb85884c816f8cfc8d957c4bd975491
Jimmy <jimhaotra1234@hotmail.com>
2021-07-05 20:06:23 +0200

    add remove function

--- database.py
+++ database.py
@@ -12,7 +12,11 @@ class Database:
 
 	def add_book(self, book):
 		self.books.append(book)
-		
+	
+	def remove_book(self, book_id):
+		for book in self.books:
+			if book._id == book_id:
+				self.books.remove(book) 
 
     # def find_book_by_author(self, query):
     #   ...
